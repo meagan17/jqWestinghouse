f5100a874d53fcea2be217f548e66445cb115bf2
meagan17 <mramo4555@withs.org>
2016-03-15 14:33:32 +0000

    updated jQuery Funhouse

--- solution.js
+++ solution.js
@@ -7,16 +7,16 @@ function switchImage(picElephant){
 }
 
 function changeBackgroundColor(){
-    $('body').css("background-color", randomColor)
+    $('body').css("background-color", randomColor);
 }
-   var safeColors = ['00','33','66','99','cc','ff'];
-var rand = function() {
+   var myColors = ['00','33','66','99','cc','ff'];
+var ran = function() {
     return Math.floor(Math.random()*6);
 };
 var randomColor = function() {
-    var r = safeColors[rand()];
-    var g = safeColors[rand()];
-    var b = safeColors[rand()];
-    return "#"+r+g+b;
+    var r = myColors[ran()];
+    var g = myColors[ran()];
+    var b = myColors[ran()];
+    return "#"+ r + g + b;
 }; 
-
+https://gist.github.com/meagan17/08401a4e7a7fd8f44193https://gist.github.com/meagan17/08401a4e7a7fd8f44193
